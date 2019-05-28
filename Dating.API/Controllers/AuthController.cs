cf164aad3fbb65bc8378947455249b265db8876b
Rony Fayyad <ronyfayyad@ronys-macbook.gracenote.gracenote.com>
2019-05-28 14:35:05 +1000

    End of section 3

---
+++ Dating.API/Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+using Dating.API.Data;
+using System.Threading.Tasks;
+using Dating.API.Models;
+using Dating.API.Dtos;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Dating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            // invalid user
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim (ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim (ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials (key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptior = new SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptior);
+
+            return Ok( new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+        
+
+    }
+}
--- Dating.API/Data/DataContext.cs
+++ Dating.API/Data/DataContext.cs
@@ -8,6 +8,7 @@ namespace Dating.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Value> Values {get; set; }
+        public DbSet<User> Users {get;set;}
 
     } 
 }
