2148291d3df9c4723b709a36eeebe9f8f0dcc879
Rony Fayyad <ronyfayyad@ronys-macbook.gracenote.gracenote.com>
2019-05-31 09:44:46 +1000

    End of section 7

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/Auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+      private authService: AuthService,
+      private router: Router,
+      private alertify: AlertifyService
+    ) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('Permission Denied');
+    this.router.navigate(['/home']);
+    return false;
+
+  }
+}
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,22 @@
+import {Routes} from '@angular/router';
+impoEnxd rt { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+// order of routing is important
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+];
